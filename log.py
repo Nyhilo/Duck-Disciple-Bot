b83fb3dea6af22bea1bdfd0dcf60e85d198e9da1
Nyhilo <silentclowd@gmail.com>
2021-03-06 16:02:49 -0700

    Update .gitignore with boilerplate

--- log.py
+++ log.py
@@ -1,5 +1,7 @@
 import logging
 
+from config import LOG_FILE
+
 # Courtesy https://stackoverflow.com/a/49562361
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
@@ -7,7 +9,7 @@ log.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(message)s')
 
 # Log to File
-fh = logging.FileHandler('log.txt', mode='w', encoding='utf-8')
+fh = logging.FileHandler(LOG_FILE, mode='w', encoding='utf-8')
 fh.setLevel(logging.DEBUG)
 fh.setFormatter(formatter)
 log.addHandler(fh)
