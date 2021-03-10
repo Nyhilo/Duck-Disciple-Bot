60d2ea45511eddbaeb2a1694149555398a3e3929
nyhilo <silentclowd@gmail.com>
2021-03-10 12:09:59 -0700

    Add error handling for user-supplied numbers

--- bot.py
+++ bot.py
@@ -34,10 +34,13 @@ async def on_ready():
 
 
 # Commands #
-@bot.command()
+@bot.command(
+    brief='Generate a random duck name.',
+    help=('Generate a random duck name.\n'
+          'Duck names are generated from the api at '
+          f'{config.NAME_GENERATOR_URL}')
+)
 async def name(ctx):
-    """Generate a random duck name."""
-
     log.info(f'Name request by {ctx.author}')
 
     name = generator.get_random_duck_name()
@@ -54,16 +57,36 @@ async def name(ctx):
 # TODO: Let command accept number to acquire duck
 # TODO: Make version of command that gets unnamed ducks as well
 # TODO: Handle possible errors
-@bot.command()
-async def duck(ctx):
-    """Fetch a random named duck from the Round 9 Gamestate wiki."""
-
+@bot.command(
+    brief='Fetch a random or specific nammed duck.',
+    help=('Fetch a random named duck from the Round 9 Gamestate wiki.\n'
+          'Use `duck <number>` to fetch a duck with a specific dice roll.')
+)
+async def duck(ctx, *, arg=None):
     log.info(f'Roll requested by {ctx.author}')
 
     cached_ducks = scraper.get_ducks(False)
     duck_count = len(cached_ducks)
 
-    random_number = randint(1, duck_count)
+    if arg is None:
+        random_number = randint(1, duck_count)
+    else:
+        try:
+            random_number = int(arg)
+        except ValueError:
+            log.info(f'Invalid value sent by user. Value: {arg}')
+            return await ctx.send(f'Value: `{arg}` is not an integer that '
+                                  'I can find a duck with.')
+
+    if arg and random_number < 1:
+        log.info(f'Number recieved is less than 1: {random_number}')
+        return await ctx.send('Please give a number greater than 0.')
+
+    if arg and random_number > duck_count:
+        log.info(f'Number received exceeds number of ducks: {random_number}')
+        return await ctx.send('The number you gave is larger than the '
+                              'number of available ducks. Please give a '
+                              f'number between 1 and {duck_count}.')
 
     msg = scraper.get_player_duck(random_number, cached_ducks, False)
 
