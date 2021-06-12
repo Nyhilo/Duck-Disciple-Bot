0c81bb1302cc7ca53eae15496497f53d9502c4f2
nyhilo <jacclowa@gmail.com>
2021-06-11 19:56:29 -0700

    Add current phase information to nomic_time

--- config.py
+++ config.py
@@ -3,3 +3,26 @@ PREFIX = '&'
 LOG_FILE = 'bot.log'
 GENERIC_ERROR = ('Whoops! An error occured while executing this command. You '
                  'should probably tell an admin about it or something...')
+
+_phase_bartering = 'Bartering Phase'
+_phase_auction = 'Auction Phase'
+
+PHASES_BY_DAY = {
+    'Monday': _phase_auction,
+    'Tuesday': _phase_auction,
+    'Wednesday': _phase_auction,
+    'Thursday': _phase_bartering,
+    'Friday': _phase_bartering,
+    'Saturday': _phase_bartering,
+    'Sunday': _phase_auction
+}
+
+PHASE_CYCLE = {
+    _phase_bartering: _phase_auction,
+    _phase_auction: _phase_bartering
+}
+
+PHASE_START = {
+    _phase_bartering: 'Thursday',
+    _phase_auction: 'Sunday'
+}
