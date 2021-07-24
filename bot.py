a96617ceb097a4eb5614d7a97110bcf1a6429ec7
nyhilo <jacclowa@gmail.com>
2021-07-24 10:52:38 -0700

    Add action for displaying an embed

--- bot.py
+++ bot.py
@@ -109,6 +109,34 @@ async def drawpairs_error(ctx, error):
         await ctx.send('Please provide an integer of cards to draw.')
 
 
+@bot.command()
+async def deck(ctx):
+    file = discord.File("img/Novice of Blades.png", filename="nob.png")
+
+    #### Create the initial embed object ####
+    embed = discord.Embed(title="Log to Console", url="javascript:console.log('hi')", description="This is an embed that will show how to build an embed and the different components.", color=0x109319)
+
+    # Add author, thumbnail, fields, and footer to the embed
+    embed.set_author(name="RealDrewData", url="https://twitter.com/RealDrewData", icon_url="https://pbs.twimg.com/profile_images/1327036716226646017/ZuaMDdtm_400x400.jpg")
+
+    embed.set_image(url="attachment://nob.png")
+
+    embed.add_field(name="Field 1 Title", value="This is the value for field 1. This is NOT an inline field.", inline=False) 
+    embed.add_field(name="Field 2 Title", value="It is inline with Field 3", inline=True)
+    embed.add_field(name="Field 3 Title", value="It is inline with Field 2", inline=True)
+
+    embed.set_footer(text="This is the footer. It contains text at the bottom of the embed")
+
+    #### Useful ctx variables ####
+    ## User's display name in the server
+    ctx.author.display_name
+
+    ## User's avatar URL
+    ctx.author.avatar_url
+
+    await ctx.send(embed=embed, file=file)
+
+
 ##############
 # Initialize #
 ##############
