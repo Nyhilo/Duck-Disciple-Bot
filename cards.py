2902415c41766545fd949b8e7523404e7cb29b57
nyhilo <jacclowa@gmail.com>
2021-08-23 11:17:05 -0700

    Change multipliers to be additive

--- cards.py
+++ cards.py
@@ -152,10 +152,10 @@ def calculate_hand_score(cards):
     # High-card constant
     # h = calculate_high_card_value(cards)
 
-    m = 0 if len(m_strs) == 0 else reduce(lambda x, y: x * y, mults, 1)
+    m = 0 if len(m_strs) == 0 else reduce(lambda x, y: x + y, mults, 1)
 
     # Return crm+h and string representation of (c*r*(m)) + h
-    mult_string = '*0' if len(m_strs) == 0 else f'*({"*".join(m_strs)})'
+    mult_string = '*0' if len(m_strs) == 0 else f'*({"+".join(m_strs)})'
     return ((c * m)), f'({c}{mult_string})'
 
     # return ((c * r * m) + h), f'({c}*{r}{mult_string}) + {h}'
