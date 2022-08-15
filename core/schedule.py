dd845bab80953507ecdc1f140e5abeffb77d2bda
nyhilo <jacclowa@gmail.com>
2022-08-15 16:55:52 -0700

    Add documentation to add_schedule() and fulfill return requirement

--- core/schedule.py
+++ core/schedule.py
@@ -39,6 +39,24 @@ def _list_schedules():
 def add_schedule(userId: int, id: str, name: str, channel: str, start_timestamp: int,
                  day_of_year: int, day_of_month: int, day_of_week: int,
                  hour_of_day: int) -> str:
+    """
+    Add a schedule oblect to the database. All arguments are required or None.
+
+    :param userId: User Id of the person running this command
+    :param id: Schedule Id. Does not need to be unique.
+    :param name: Human-readable hepler-name
+    :param channel: Channel Id to send schedule to
+    :param start_timestamp: UTC timestamp to start calculating schedule from
+    :param day_of_year: 1-365. Day of the year a schedule should trigger
+    :param day_of_month: 1-31. Day of the month a schedule should trigger
+    :param day_of_week: 1-7. Day of the week a schedule should trigger
+    :param hour_of_day: 0-23. Hour of the day a schedule should trigger. This
+                         only modifies the time of day the previous constraints
+                         trigger at. For example, setting the hour to 5 will
+                         cause the schedule to trigger at 5am on every day it is
+                         marked to trigger for this schedule object.
+    :return: A message of confirmation that the addition was successful
+    """
 
     if not utils.is_admin(userId):
         return None
@@ -54,4 +72,7 @@ def _add_schedule(id: str, name: str, channel: str, start_timestamp: int,
     schedule = Schedule(id, name, channel, start_timestamp, day_of_year,
                         day_of_month, day_of_week, hour_of_day)
 
-    db.add_schedule(schedule)
+    success = db.add_schedule(schedule)
+
+    _not = ' not' if not success else ''
+    return f'Schedule has{_not} been added.'
