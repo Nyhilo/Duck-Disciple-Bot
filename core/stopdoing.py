157e613bc86ec0ac5f525b9f8bfb8115c2bc91fa
nyhilo <jacclowa@gmail.com>
2022-11-25 12:42:24 -0700

    Add Stop Doing Nixos

--- core/stopdoing.py
+++ core/stopdoing.py
@@ -98,6 +98,7 @@ class StopDoing():
             Option(send_image, 1, 'stop doing2.jpg', [rf'stop doing{punc}$', nomic]),
             Option(send_image, 1, 'stop doing3.jpg', [rf'stop doing{punc}$', nomic]),
             Option(send_image, 10, 'stop doing logic.jpg', rf'{stop} doing logic'),
+            Option(send_image, 5, 'stop doing nixos.png', rf'{stop} doing nixos'),
             Option(send_image, 15, 'stop doing computer science.png',
                    rf'{stop} doing (compsci|computer science|programming|coding)'),
             Option(send_image, 5, 'how can he do this without drowning.jpg',
