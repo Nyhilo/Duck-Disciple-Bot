67c5c9d185c94eff4d0a5139560f9ffb9eab1e19
nyhilo <jacclowa@gmail.com>
2022-12-22 20:20:20 -0700

    It is now Loop 5 (that was quick)

--- config/config.py
+++ config/config.py
@@ -122,9 +122,9 @@ UTC_UPDATE_CHANNEL = 1029079234427244544 if not DEBUG else 1028894151451885638
 PHASE_UPDATE_CHANNEL = 1029235284195418192 if not DEBUG else 1029220076097896448
 PHASE_END_UPDATE_CHANNEL = 1033465614695673858 if not DEBUG else 1031640532939702363
 
-PHASE_START_DATE = (2022, 12, 21)
-LOOP_NAME = 'Fourth Loop'
+PHASE_START_DATE = (2022, 12, 23)
+LOOP_NAME = 'Fifth Loop'
 
 # Defines the cycle in which phases are looped starting with the start date
 # For instance, a cycle of [3, 2, 2] would have a 3-day Phase I, 2-day Phase II, 2-day Phase III, etc.
-PHASE_GROUPS = [2, 2, 3]
+PHASE_GROUPS = [2, 3, 2]
