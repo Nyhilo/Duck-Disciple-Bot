1e7047aa8cb2f47deb64985738b0cafa7adee0c6
nyhilo <jacclowa@gmail.com>
2023-04-03 19:34:00 -0700

    Create settings.py that uses @property to manage settings_db settings

--- core/language.py
+++ core/language.py
@@ -1,26 +1,23 @@
-from typing import List
 from os import path
 import json
 
 from core.log import log
-import core.db.settings_db as db
+from config.settings import settings
 
 _langfile_directory = './langs/'
 _langfile_extension = '.json'
 
-_locale_db_key = 'current_locale_key'
 
 class Locale():
     # TODO: Put default locale in config
     default_key = 'en-US'
     current_key = None
-    
+
     default_locale = None
     current_locale = None
-    
+
     def __init__(self, base_path) -> None:
         self.base_path = base_path
-
 
     def initialize(self) -> None:
         '''Initialize the static application locales. This function should only be called once.'''
@@ -29,22 +26,19 @@ class Locale():
         self.set_default_locale(self.default_key)
 
         # Set current locale if it exists in the database, set it to the default otherwise
-        locale_key = db.get_setting(_locale_db_key)
+        locale_key = settings.current_locale_key
         if locale_key is None:
             locale_key = self.default_key
-        
+
         self.set_locale(locale_key)
-    
 
-    def set_locale(self, locale_key:str) -> str:
+    def set_locale(self, locale_key: str) -> str:
         return self._set_locale(locale_key, False)
-    
 
-    def set_default_locale(self, locale_key:str) -> str:
+    def set_default_locale(self, locale_key: str) -> str:
         return self._set_locale(locale_key, True)
 
-
-    def _set_locale(self, locale_key:str, set_default) -> str:
+    def _set_locale(self, locale_key: str, set_default) -> str:
         # Check if locale file exists
         filepath = f'{_langfile_directory}{locale_key}{_langfile_extension}'
 
@@ -54,18 +48,17 @@ class Locale():
         # Load the file into memory
         with open(filepath, 'r') as f:
             if set_default:
-                self._set_default_locale( json.load(f) )
-                self._set_default_key( locale_key )
+                self._set_default_locale(json.load(f))
+                self._set_default_key(locale_key)
                 log.info(f'Default locale set to {self.default_key}')
             else:
-                self._set_current_locale( json.load(f) )
-                self._set_current_key( locale_key )
-                db.save_setting(_locale_db_key, locale_key)
+                self._set_current_locale(json.load(f))
+                self._set_current_key(locale_key)
+                settings.current_locale_key = locale_key
                 log.info(f'Current locale set to {locale_key}')
-        
+
         # And we're golden I guess
         self.current_key = locale_key
-
 
     def get_string(self, path, default_lang=False, **formatkwargs):
         """Combine base path with args path to find correct string for key
@@ -84,7 +77,7 @@ class Locale():
                               terminate on a string
 
         :return:             The formatted string in the json at the target path
-        """                
+        """
 
         fullkey = f'{self.base_path}.{path}'
         fullpath = str.split(fullkey, '.')
@@ -96,14 +89,14 @@ class Locale():
                 node = node[step]
 
             assert(type(node) is str)
-        
+
         # The path is not valid, but may exist in the default lang file
         except KeyError:
             if not default_lang:
                 return self.get_string(path, default_lang=True, **formatkwargs)
-            
+
             raise KeyError(f'Path does not exist in lang file: {fullkey}')
-        
+
         # The given key is valid, but leads to a higher level of nesting
         except AssertionError:
             raise KeyError(f'Key does not lead to a string: {fullkey}')
@@ -111,19 +104,18 @@ class Locale():
         # If the node is validated as a string, then we can return it
         return node.format(**formatkwargs)
 
+    @classmethod
+    def _set_default_key(cls, key: str) -> None:
+        cls.default_key = key
 
     @classmethod
-    def _set_default_key(cls, key:str) -> None:
-        cls.default_key = key
-    
-    @classmethod
-    def _set_current_key(cls, key:str) -> None:
+    def _set_current_key(cls, key: str) -> None:
         cls.current_key = key
-    
+
     @classmethod
-    def _set_default_locale(cls, locale:dict) -> None:
+    def _set_default_locale(cls, locale: dict) -> None:
         cls.default_locale = locale
-    
+
     @classmethod
-    def _set_current_locale(cls, locale:dict) -> None:
+    def _set_current_locale(cls, locale: dict) -> None:
         cls.current_locale = locale
